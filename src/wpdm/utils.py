f5fa7f4e578f684826d9ee0786ce2fd9f7186cfc
Grubysamuraj <jakub.dragosz@o2.pl>
2024-03-27 00:50:45 +0100

    commit

--- src/wpdm/utils.py
+++ src/wpdm/utils.py
@@ -1,8 +1,22 @@
-def foo(x,y):
+import pandas as pd
+import numpy as np
+
+
+def foo(x, y):
     """
 
     :param x:
     :param y:
     :return:
     """
-    return x+y
+    return x - y
+
+
+def printuj(f):
+    print(f)
+
+
+def fillMissingData(df):
+    df.set_index('id')
+    df.replace('?', np.nan, inplace=True)
+    df
